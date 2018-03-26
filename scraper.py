fb5e17a92e20fc819186de0fad1fe4333d0381c6
Aaron Zipp <aaronzipp@protonmail.com>
2018-03-26 17:35:23 +0200

    Added asynchronous scraping

--- scraper.py
+++ scraper.py
@@ -1,86 +1,102 @@
 import os
 import time
 import re
+import asyncio
 import requests
 from bs4 import BeautifulSoup
-from pytube import YouTube
 from operator import itemgetter
+from pytube import YouTube
 
 from subscriptions import channels
 
-# needs to be modified for other languages
-dates = {	"Sekunde": 1, "Sekunden": 1, "Minute": 60, "Minuten": 60, "Stunde": 3600, 
+async def main():
+	
+	start = time.time()
+
+	dates = {	"Sekunde": 1, "Sekunden": 1, "Minute": 60, "Minuten": 60, "Stunde": 3600, 
 			"Stunden": 3600, "Tag": 86400, "Tagen": 86400, "Woche": 604800, "Wochen": 604800, 
 			"Monat": 2592000, "Monaten": 2592000, "Jahr": 31104000, "Jahren": 31104000}
 
-destination = "C:\\Users\\aaron\\Videos\\YouTube"
+	destination = "C:\\Users\\aaron\\Videos\\YouTube"
 
-videos = []
+	loop = asyncio.get_event_loop()
 
-start = time.time()
+	videos = []
 
-for channel in channels:
-	channel_name = channel['channel']
-	channel_id = channel['id']
+	ch_max = len(channels)
+	ch = 0
 
-	r = requests.get("https://www.youtube.com/channel/{}/videos".format(channel_id))
+	req = ["" for i in range(ch_max)]
+	site = [{} for i in range(ch_max)]
 
-	soup = BeautifulSoup(r.text, 'html.parser')
-
-	title = soup.select('h3.yt-lockup-title')
-	meta = soup.select('div.yt-lockup-meta')
-
-	# Title, (relative) link; location of strip needs to be modified for other languages
-	video_attr = [[vid.a.attrs['title'],vid.a.attrs['href']] for vid in title]
-
-	# Age; location of strip needs to be modified for other languages
-	description = [m.ul.li.next_sibling.string[4:] for m in meta]
+	for i in range(ch_max):
+		url = "https://www.youtube.com/channel/{}/videos".format(channels[i]['id'])
+		req[i] = loop.run_in_executor(None, requests.get, url)
 	
-	age = [0 for i in range(len(description))]
-	for i in range(len(description)):
-		number, period = description[i].split()
-		age[i] = int(number) * dates[period]
-
-	videos += [{"channel": channel_name,
-				"title": re.sub('[|,.:;#\'"$%/?\\*~]', '', video_attr[i][0]),
-				"link": video_attr[i][1],
-				"age": age[i]} for i in range(len(video_attr))]
-				
-
-sorted_videos = sorted(videos, key=itemgetter('age'))[:80]
-sorted_videos = sorted_videos[::-1]
-
-# delete videos that aren't in the list anymore
-offline_videos = [video for video in os.listdir(destination) if os.path.isfile("{}\\{}".format(destination,video))]
-vids_deleted = 0
-
-for i in range(len(offline_videos)):
-	vid, ext = os.path.splitext(offline_videos[i])
-	is_in_list = False
-	
-	for list_vid in sorted_videos:
-		if vid == list_vid['title']:
-			is_in_list = True
-			break
-	if not is_in_list:
-		vids_deleted += 1
-		print("Deleting: \"{}{}\"\n".format(vid,ext))
-		os.remove("{}\\{}{}".format(destination,vid,ext))		
-
-vids_downloaded = 0
-for vid in sorted_videos:
-	if not os.path.isfile("{}/{}.mp4".format(destination,vid['title'])):
-		try: # check if video is livestream
-			yt = YouTube("https://youtube.com{}".format(vid['link']))
-		except:
-			continue
-		vids_downloaded += 1
-		print("[{:2}] Downloading: \"{}\" by {}\n".format(vids_downloaded, vid['title'], vid['channel']))
-		download_start = time.time()
-		yt.streams.filter(subtype='mp4').first().download(destination)
-		download_end = time.time()
-		print("Download finished in {:0.0f} seconds\n".format(download_end-download_start))
+	for i in range(ch_max):
+		site[i] = [await req[i], channels[i]['channel']]
 		
-end = time.time()		
-print("Elapsed time: {:0.0f} seconds. {} videos downloaded and {} videos deleted".format(end-start, vids_downloaded, vids_deleted))
+		soup = BeautifulSoup(site[i][0].text, 'html.parser')
 
+		title = soup.select('h3.yt-lockup-title')
+		meta = soup.select('div.yt-lockup-meta')
+
+		video_attr = [[vid.a.attrs['title'],vid.a.attrs['href']] for vid in title]
+
+		description = [m.ul.li.next_sibling.string[4:] for m in meta]
+	
+		age = [0 for j in range(len(description))]
+		for j in range(len(description)):
+			number, period = description[j].split()
+			age[j] = int(number) * dates[period]
+
+		videos += 	[{"channel": site[i][1],
+				"title": re.sub('[|,.:;#\'"$%/?\\*~]', '', video_attr[k][0]),
+				"link": video_attr[k][1],
+				"age": age[k]} for k in range(len(video_attr))]
+		ch += 1
+		print("Scanning: {:.0%}".format(ch/ch_max),end='\r')
+	
+	sorted_videos = sorted(videos, key=itemgetter('age'))[:80]
+	sorted_videos = sorted_videos[::-1]
+
+	offline_videos = [video for video in os.listdir(destination) if os.path.isfile("{}\\{}".format(destination,video))]
+	vids_deleted = 0
+
+	for i in range(len(offline_videos)):
+		vid, ext = os.path.splitext(offline_videos[i])
+		is_in_list = False
+		
+		for list_vid in sorted_videos:
+			if vid == list_vid['title']:
+				is_in_list = True
+				break
+		if not is_in_list:
+			vids_deleted += 1
+			print("Deleting: \"{}{}\"\n".format(vid,ext))
+			os.remove("{}\\{}{}".format(destination,vid,ext))		
+	
+	vids_downloaded = 0
+	for vid in sorted_videos:
+		if not os.path.isfile("{}/{}.mp4".format(destination,vid['title'])):
+			try: # check if video is livestream
+				yt = YouTube("https://youtube.com{}".format(vid['link'])).streams.filter(subtype='mp4').first()
+			except:
+				continue
+
+			vids_downloaded += 1
+			print("[{:2}] Downloading: \"{}\" by {} ({} MB)\n".format(vids_downloaded, vid['title'], vid['channel'], round(yt.filesize/(2**20),1)))
+
+			download_start = time.time()
+			yt.download(destination)
+			download_end = time.time()
+
+			print("Download finished in {:0.0f} seconds\n".format(download_end-download_start))
+			
+	end = time.time()		
+	print("Elapsed time: {:.0f} seconds. {} videos downloaded and {} videos deleted".format(end-start, vids_downloaded, vids_deleted))
+
+
+if __name__ == '__main__':
+	loop = asyncio.get_event_loop()
+	loop.run_until_complete(main())
