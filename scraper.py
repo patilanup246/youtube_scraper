37db1251bf5832cecd0db41a15143410c3a28240
Aaron Zipp <aaronzipp@protonmail.com>
2018-05-19 13:34:26 +0200

    Improved prog bar & modified path

--- scraper.py
+++ scraper.py
@@ -37,43 +37,44 @@ async def main():
                     'date': feed[i].published} for i in range(len(feed))]
         ch += 1
         progress = int(ch/ch_max*100)
-        bar = '█'*(progress//5) + ' '*(20-progress//5)
-        print(f"Scanning: {bar} {progress/100:.0%}",end='\r')
+        prog_bar = progress//2
+        bar = '█'*(prog_bar) + ' '*(50-prog_bar)
+        print(f"Scanning: {bar} {progress/100:.0%}\t",end='\r')
 
     videos = sorted(videos, key=itemgetter('date'))[-num_videos:]
 
-    offline_videos = [video for video in os.listdir(destination) if os.path.isfile(f"{destination}\\{video}")]
+    offline_videos = [video for video in os.listdir(destination) if os.path.isfile(f"{destination}/{video}")]
 
     for offline_vid in offline_videos:
         vid, ext = os.path.splitext(offline_vid)
         is_in_list = False
 
-        for list_vid in videos:
+        for i, list_vid in enumerate(videos):
             if vid == f"[{list_vid['channel']}] " + list_vid['title']:
                 is_in_list = True
+                videos.pop(i)
                 break
         if not is_in_list:
             vids_deleted += 1
             print(f"Deleting: \"{vid}{ext}\"\n")
-            os.remove(f"{destination}\\{vid}{ext}")
+            os.remove(f"{destination}/{vid}{ext}")
 
     for vid in videos:
-        if not os.path.isfile(f"{destination}/[{vid['channel']}] {vid['title']}.mp4"):
-            try:
-                yt = YouTube(f"https://youtube.com{vid['link']}").streams.filter(subtype='mp4').first()
-            except:
-                continue
+        try:
+            yt = YouTube(f"https://youtube.com{vid['link']}").streams.filter(subtype='mp4').first()
+        except:
+            continue
 
-            vids_downloaded += 1
-            print(f"[{vids_downloaded:2}] Downloading: \"{vid['title']}\" by {vid['channel']} ({round(yt.filesize/(2**20),1)} MB)\n")
+        vids_downloaded += 1
+        print(f"[{vids_downloaded:2}] Downloading: \"{vid['title']}\" by {vid['channel']}\n")
 
-            download_start = time.time()
-            yt.download(destination)
-            video_file = File(f"{destination}\\{vid['title']}.mp4")
-            video_file.rename(f"[{vid['channel']}] {vid['title']}.mp4")
-            download_end = time.time()
+        download_start = time.time()
+        yt.download(destination)
+        video_file = File(f"{destination}/{vid['title']}.mp4")
+        video_file.rename(f"[{vid['channel']}] {vid['title']}.mp4")
+        download_end = time.time()
 
-            print(f"Download finished in {download_end-download_start:0.0f} seconds\n")
+        print(f"Download finished in {download_end-download_start:0.0f} seconds\n")
 
     end = time.time()
     print(f"Elapsed time: {end-start:.0f} seconds. {vids_downloaded} videos downloaded and {vids_deleted} videos deleted")
